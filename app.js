5f62d7f655f79cad8b4ee9e1a8b8c949adb011a5
Nalinsharma7 <108210732+Nalinsharma7@users.noreply.github.com>
2022-11-15 23:06:26 +0530

    first

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express();
+const PORT = 5000;
+const mongoose = require('mongoose')
+const {MONGOURI} = require('./keys')
+//G5XuknS5LiWICamV
+
+mongoose.connect(MONGOURI)
+
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to mongo')
+})
+mongoose.connection.on('error',(err)=>{
+    console.log('Error in connecting to mongo',err)
+})
+
+const customMiddleware = (req,res,next)=>{
+    console.log("Middleware Fired!!")
+    next()
+}
+
+// app.use(customMiddleware);
+
+app.get('/',(req,res)=>{
+    res.send("HELLO WORLD")
+})
+
+app.get('/about',customMiddleware,(req,res)=>{
+    console.log("About")
+    res.send("About")
+})
+
+app.listen(PORT,()=>{
+    console.log("RUNNING ON 5000")
+})
---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGOURI:"mongodb+srv://nalin:G5XuknS5LiWICamV@cluster0.swi39qw.mongodb.net/?retryWrites=true&w=majority"
+
+}
