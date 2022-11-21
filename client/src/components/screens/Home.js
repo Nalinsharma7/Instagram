62af1bcfa94422ffa13737751e8e596902de7667
Nalinsharma7 <108210732+Nalinsharma7@users.noreply.github.com>
2022-11-21 11:48:02 +0530

    home

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,47 @@ import React from 'react'
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <div className="home">
+      <div className="card home-card">
+        <h5>Nalin</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+        </div>
+        <div className="card-content">
+        <i className="material-icons">favorite</i>
+          <h6>title</h6> 
+          <p>This is fine!</p>
+          <input type="text" placeholder="Add comment"/>
+        </div>
+      </div>
+      
+      <div className="card home-card">
+        <h5>Nalin</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+        </div>
+        <div className="card-content">
+        <i class="material-icons">favorite</i>
+          <h6>title</h6> 
+          <p>This is fine!</p>
+          <input type="text" placeholder="Add comment"/>
+        </div>
+      </div>
+      
+      <div className="card home-card">
+        <h5>Nalin</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+        </div>
+        <div className="card-content">
+        <i class="material-icons">favorite</i>
+          <h6>title</h6> 
+          <p>This is fine!</p>
+          <input type="text" placeholder="Add comment"/>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
