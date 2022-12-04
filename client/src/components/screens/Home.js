22539d5864f96b5a02feefb8be431102534cf0d3
Nalinsharma7 <nalinsharma606@gmail.com>
2022-12-04 23:21:02 +0530

    idk

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -96,6 +96,22 @@ function Home() {
         console.log(err)
       })
   }
+
+  const deletePost = (postId) => {
+    fetch(`/deletepost/${postId}`, {
+      method: "delete",
+      headers: {
+        Authorization: "Bearer " +localStorage.getItem("jwt")
+      }
+    }).then(res => res.json())
+      .then(result => {
+        console.log(result)
+        const newData = data.filter(item => {
+          return item._id !== result._id
+        })
+        setData(newData)
+      })
+  }
   
 
 
@@ -105,7 +121,12 @@ function Home() {
         data.map(item => {
           return (
             <div className="card home-card" key={item._id}>
-        <h5>{item.postedBy.name}</h5>
+              <h5>{item.postedBy.name}  {item.postedBy._id == state.state._id &&<i className="material-icons" style={{
+                float:"right"
+              }}
+              onClick={()=>deletePost(item._id)}
+              >delete</i>
+              }</h5>
         <div className="card-image">
                 <img src={ item.photo} />
         </div>
